7917d6c88d8f8725c3b0f5a0a9893087f6efa935
sidhartha saxena <sidd.saxena96@gmail.com>
2018-02-23 22:05:49 +0530

    no message

---
+++ Assets/Scripts/StarText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarText : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text> ().text = ""+GetComponentInParent<StarBehaviour> ().value;
+	}	
+}
